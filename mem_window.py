21618ae2bb435e8ee987392a90e703b411d9e257
gdgng01@gmail.com <79651007+gdgng@users.noreply.github.com>
2025-06-21 00:07:12 +0200

    Delete mem_window.py

--- mem_window.py
+++
@@ -1,8 +0,0 @@
-import webview
-def get_title():
-    return window.evaluate_js("document.title")
-
-window = webview.create_window("Live View Mempool - Tracker", "https://www.mempool.space")
-webview.start()
-
-# print(get_title())  # Fetches the webpage title
